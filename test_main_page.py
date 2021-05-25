9befe033c5ca37b3e52ad1824b9ad8628deb14c1
AkioYuki <meow.fire.meow@gmail.com>
2021-05-25 23:15:08 +0300

    add login_page and locators

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,23 @@
-from .pages.main_page import MainPage
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+    main_page = MainPage(browser, link)
+    main_page.open()
+    main_page.should_be_login_link()
+
+
+def test_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_login_url()
