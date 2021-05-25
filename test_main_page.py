32b680f8f29d9a2772d5904491a93d4398bf3e9c
AkioYuki <meow.fire.meow@gmail.com>
2021-05-25 23:33:16 +0300

    in the test added transitions between pages

--- test_main_page.py
+++ test_main_page.py
@@ -3,21 +3,18 @@ from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+    link = "http://selenium1py.pythonanywhere.com/en-gb/" \
+           "catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+    main_page = MainPage(browser, link)
+    login_page = LoginPage(browser, browser.current_url)
+    main_page.open()
+    main_page.go_to_login_page()
+    login_page.should_be_login_url()
 
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=midsummer"
+    link = "http://selenium1py.pythonanywhere.com/en-gb/" \
+           "catalogue/the-shellcoders-handbook_209/?promo=midsummer"
     main_page = MainPage(browser, link)
     main_page.open()
     main_page.should_be_login_link()
-
-
-def test_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    login_page = LoginPage(browser, link)
-    login_page.open()
-    login_page.should_be_login_url()
